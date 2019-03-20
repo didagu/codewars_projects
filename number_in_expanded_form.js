308db74fadf93bd044458e2f395360931d14f316
Dominic Idagu <didagou@gmail.com>
2019-03-20 18:57:40 +0100

    Refractor code for number_in_expanded_form

--- number_in_expanded_form.js
+++ number_in_expanded_form.js
@@ -1,16 +1,16 @@
-function expandedForm(num) {
+function expandedForm(number) {
     // Your code here
-    var res = []
-    var numArr = num.toString().split('')
-    for(var i = 1; i <= numArr.length ; i++){
-        if(numArr[i-1] != 0){
-            var exp = numArr.length - i
-            var value = Math.pow(10,exp) * numArr[i-1]
+    var result = []
+    var numberArray = number.toString().split('')
+    for(var i = 1; i <= numberArray.length ; i++){
+        if(numberArray[i-1] != 0){
+            var exponent = numberArray.length - i
+            var value = Math.pow(10,exponent) * numberArray[i-1]
             var stringValue = value.toString()
-            res.push(stringValue)
+            result.push(stringValue)
         }            
     }
-    return res.join(' + ')
+    return result.join(' + ')
 }
 
 console.log(expandedForm(12), '10 + 2');
