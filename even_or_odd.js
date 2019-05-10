521ed42f23e7e305fc2a7d775dccd2214f84fe8d
didagu <didagou@gmail.com>
2019-05-10 14:33:08 +0200

    Add solution even_or_odd

---
+++ even_or_odd.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+  // ...
+  return (number % 2 === 0) ? "Even": "Odd"
+}
+
+console.log(even_or_odd(2), "Even")
+console.log(even_or_odd(0), "Even")
+console.log(even_or_odd(7), "Odd")
+console.log(even_or_odd(1), "Odd")
